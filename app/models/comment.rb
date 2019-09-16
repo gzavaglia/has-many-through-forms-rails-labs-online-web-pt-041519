b15f2eae6d074696740a507b822a0ca7dfdb8fee
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-09-16 17:30:46 +0000

    Automatically backed up by Learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-def user_attributes(user_attributes)
+  def user_attributes(user_attributes)
     user_attributes.values.each do |user_attributes|
       if user_attributes[:username].present?
         user = User.find_or_create_by(user_attributes)
